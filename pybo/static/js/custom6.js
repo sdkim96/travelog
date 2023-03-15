a8f3d87477b5ff39f221451a5256703bb1f607e7
JunSeop Kim <wnqtjq8958@naver.com>
2023-03-15 20:53:49 +0900

    refactor : 개행 수정

--- pybo/static/js/custom6.js
+++ pybo/static/js/custom6.js
@@ -1,9 +1,9 @@
 var params = {
-  container: document.querySelector("#lottie6"),
-  renderer: "canvas",
-  loop: true,
-  autoplay: true,
-  path: "static/js/FAQ.json",
+    container: document.querySelector("#lottie6"),
+    renderer: "canvas",
+    loop: true,
+    autoplay: true,
+    path: "static/js/FAQ.json",
 };
 
 var anim;
--- pybo/static/js/js_PoseNet.js
+++ pybo/static/js/js_PoseNet.js
@@ -6,11 +6,11 @@ var ctx = canvas.getContext("2d");
 let poses = [];
 
 // Create a webcam capture
-document.querySelector('.start').addEventListener('click', function(){
+document.querySelector('.start').addEventListener('click', function () {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-          video.srcObject = stream;
-          video.play();
+        navigator.mediaDevices.getUserMedia({video: true}).then(function (stream) {
+            video.srcObject = stream;
+            video.play();
         });
     }
 })
@@ -21,13 +21,14 @@ document.querySelector('.start').addEventListener('click', function(){
 // This way the video will not seem slow if poseNet
 // is not detecting a position
 function drawCameraIntoCanvas() {
-  // Draw the video element into the canvas
-  ctx.drawImage(video, 0, 0, 640, 480);
-  // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
-  window.requestAnimationFrame(drawCameraIntoCanvas);
+    // Draw the video element into the canvas
+    ctx.drawImage(video, 0, 0, 640, 480);
+    // We can call both functions to draw all keypoints and the skeletons
+    drawKeypoints();
+    drawSkeleton();
+    window.requestAnimationFrame(drawCameraIntoCanvas);
 }
+
 // Loop over the drawCameraIntoCanvas function
 drawCameraIntoCanvas();
 
@@ -37,46 +38,46 @@ poseNet.on("pose", gotPoses);
 
 // A function that gets called every time there's an update from the model
 function gotPoses(results) {
-  poses = results;
+    poses = results;
 }
 
 function modelReady() {
-  console.log("model ready");
-  poseNet.multiPose(video);
+    console.log("model ready");
+    poseNet.multiPose(video);
 }
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
-      let keypoint = poses[i].pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        ctx.beginPath();
-        ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
-        ctx.fillStyle = '#92f7fc';
-        ctx.fill();
-      }
+    // Loop through all the poses detected
+    for (let i = 0; i < poses.length; i += 1) {
+        // For each pose detected, loop through all the keypoints
+        for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
+            let keypoint = poses[i].pose.keypoints[j];
+            // Only draw an ellipse is the pose probability is bigger than 0.2
+            if (keypoint.score > 0.2) {
+                ctx.beginPath();
+                ctx.arc(keypoint.position.x, keypoint.position.y, 10, 0, 2 * Math.PI);
+                ctx.fillStyle = '#92f7fc';
+                ctx.fill();
+            }
+        }
     }
-  }
 }
 
 // A function to draw the skeletons
 function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < poses[i].skeleton.length; j += 1) {
-      let partA = poses[i].skeleton[j][0];
-      let partB = poses[i].skeleton[j][1];
-      ctx.beginPath();
-      ctx.moveTo(partA.position.x, partA.position.y);
-      ctx.lineTo(partB.position.x, partB.position.y);
-      ctx.strokeStyle = '#92f7fc';
-      ctx.lineWidth = 5;
-      ctx.stroke();
+    // Loop through all the skeletons detected
+    for (let i = 0; i < poses.length; i += 1) {
+        // For every skeleton, loop through all body connections
+        for (let j = 0; j < poses[i].skeleton.length; j += 1) {
+            let partA = poses[i].skeleton[j][0];
+            let partB = poses[i].skeleton[j][1];
+            ctx.beginPath();
+            ctx.moveTo(partA.position.x, partA.position.y);
+            ctx.lineTo(partB.position.x, partB.position.y);
+            ctx.strokeStyle = '#92f7fc';
+            ctx.lineWidth = 5;
+            ctx.stroke();
+        }
     }
-  }
 }
